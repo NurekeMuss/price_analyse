897a6ec3548713668c665a839df598441c445696
yerassyl.omirtay <yerassyl.omirtay@targetai.kz>
2025-05-16 02:08:05 +0500

    rename

--- backend/src/dependencies/potd.py
+++ backend/src/dependencies/potd.py
@@ -1,6 +1,6 @@
 from src.ml.potd import ProductOfTheDayClassifier
 
-_classifier = ProductOfTheDayClassifier('./backend/src/ml/data/mock_products.csv')
+_classifier = ProductOfTheDayClassifier('./backend/src/ml/train_data/mock_products.csv')
 
 def get_product_of_the_day_classifier() -> ProductOfTheDayClassifier:
     return _classifier
